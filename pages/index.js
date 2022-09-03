b3eb8886b15eac682bd201d0c9a54e2303d1e500
joshua constine <joshua.constine97@gmail.com>
2022-09-03 14:15:02 -0700

    made lib

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,12 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/Banner";
 import NavBar from "../components/nav/Navbar";
-import Card from "../components/card/Card";
 import SectionCards from "../components/card/SectionCards";
+import { getVideos } from "../lib/videos";
+
 export default function Home() {
-  const disneyVideos = [
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-    {
-      imgUrl: "/static/clifford.webp",
-    },
-  ];
+  const disneyVideos = getVideos();
   return (
     <div className={styles.container}>
       <Head>
