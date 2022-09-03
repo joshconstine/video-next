5d5222e630151b8a75ebed833e37fcd6074ca558
joshua constine <joshua.constine97@gmail.com>
2022-09-03 13:07:12 -0700

    added framer motion

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./card.module.css";
 import { useState } from "react";
+import { motion } from "framer-motion";
+import cls from "classnames";
 const Card = (props) => {
   const {
     imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
@@ -21,7 +23,10 @@ const Card = (props) => {
   return (
     <div className={styles.container}>
       Card
-      <div className={classMap[size]}>
+      <motion.div
+        className={cls(styles.imgMotionWrapper, classMap[size])}
+        whileHover={{ scale: 1.2 }}
+      >
         <Image
           src={imgSrc}
           alt="image"
@@ -29,7 +34,7 @@ const Card = (props) => {
           className={styles.cardImg}
           onError={handleOnError}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
